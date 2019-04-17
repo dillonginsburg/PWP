bee33dab4293c0adc54d2597615db7363ffd932b
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-17 16:41:02 -0600

    commit2 milestone-1 php file

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -11,18 +11,26 @@
 			<li><strong>Purpose:</strong>  The purpose of this web page is to showcase my newfound web develpement skills.</li>
 			<li><strong>Audience:</strong>  This site is fashioned for people and organizations around the world looking to
 				integrate a web developer into their team.  </li>
-			<li><strong>Goal:</strong>  The goal of this web page is to give those who are need of web development the ability to contact me, and a reason to do so. </li>
+			<li><strong>Goal:</strong>  The goal of this web page is to give those who are need of web development the
+				ability to contact me, and a reason to do so. </li>
 		</ol>
 		</div>
 
 		<h1>Persona</h1>
 		<ul>
-			<li><strong>Name:</strong></li>
-			<li><strong>Age:</strong></li>
-			<li><strong>Profession:</strong></li>
-			<li><strong>Technology:</strong></li>
-			<li><strong>Attitudes and Behaviors:</strong></li>
-			<li><strong>Frustrations and Needs:</strong></li>
+			<li><strong>Name:</strong>Howard Stern</li>
+			<li><strong>Age:</strong>42</li>
+			<li><strong>Profession:</strong>IT Recruitment for Wikipedia. Looking for a web developer to work part-time on
+				maintenance of existing pages, and the creation of new ones.</li>
+			<li><strong>Technology:</strong>Power Linux user. Powerful custom built workhorse of a desktop at both home and
+				office. Samsung Note phone with unlimited data through a 4GLTE network.</li>
+			<li><strong>Attitudes and Behaviors:</strong>Calm, focused, and driven. Doesn't need an immediate hire, but
+				somebody competent that believes in the worldly benefit of their work.</li>
+			<li><strong>Frustrations and Needs:</strong>Howard needs to find somebody to help with the ever-growing load of
+				maintaining enormous databases and websites. He is willing to do some training, but the ideal candidate
+				would be seasoned in both front-end and back-end web design. Because funding is always so tight, candidates
+				need to be more driven by moral motives than fiscal, though starting salary for this position would be on
+				par with the average for entry-level web developing.</li>
 			<li><strong>Goals:</strong></li>
 			<li><strong>User Story:</strong></li>
 
