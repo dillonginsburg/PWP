1f291a4187a3c234d8d341f844421a1520963e78
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-17 15:17:37 -0600

    hopeful commit to add gitignore

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,1 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PWP</title>
+	</head>
+	<body>
+		<div>
+		<h1>Purpose, Audience & Goal</h1>
+		<ol>
+			<li><strong>Purpose:</strong>  The purpose of this web page is to showcase my newfound web develpement skills.</li>
+			<li><strong>Audience:</strong>  This site is fashioned for people and organizations around the world looking to
+				integrate a web developer into their team.  </li>
+			<li><strong>Goal:</strong>  The goal of this web page is to give those who are need of web development the ability to contact me, and a reason to do so. </li>
+		</ol>
+		</div>
+
+		<h1>Persona</h1>
+		<ul>
+			<li><strong>Name:</strong></li>
+			<li><strong>Age:</strong></li>
+			<li><strong>Profession:</strong></li>
+			<li><strong>Technology:</strong></li>
+			<li><strong>Attitudes and Behaviors:</strong></li>
+			<li><strong>Frustrations and Needs:</strong></li>
+			<li><strong>Goals:</strong></li>
+			<li><strong>User Story:</strong></li>
+
+		</ul>
+	</body>
+
+</html>
