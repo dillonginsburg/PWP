81ba663052f936b456898d68b4145f810bfafe48
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-25 16:28:41 -0600

    phase 2, commit 1

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PWP</title>
+	</head>
+	<div>
+		<h1>Content Strategy</h1>
+		<p>My site will be one page and consist of the following sections:
+		</p>
+		<ul>
+			<li><em>Opening:</em> Title and subtitle, probably. It will declare my name and my occupation.</li>
+			<li><em>About Me:</em> One paragraph stating my skills and my goals</li>
+			<li><em>Portfolio:</em> This will be an image carousel showcasing my projects. Each image will link to the actual page.</li>
+			<li><em>Contact Me:</em> an link to an image of my resume, and my email. icon links for twitter, faceboook, and linkedin</li>
+		</ul>
+	</div>
