06c6ca7a9b9e5d211a2a9afc4a61cadb38ca3797
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-05-02 12:47:26 -0600

    milestone 2a

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -12,6 +12,9 @@
 			<li><em>Opening:</em> Title and subtitle, probably. It will declare my name and my occupation.</li>
 			<li><em>About Me:</em> One paragraph stating my skills and my goals</li>
 			<li><em>Portfolio:</em> This will be an image carousel showcasing my projects. Each image will link to the actual page.</li>
-			<li><em>Contact Me:</em> an link to an image of my resume, and my email. icon links for twitter, faceboook, and linkedin</li>
+			<li><em>Contact Me:</em> link to an image of my resume and my email. icon links for twitter, faceboook, and linkedin</li>
 		</ul>
+		<img src ="New%20Wireframe%201.png">
+		<img src ="New%20Wireframe%202.png">
 	</div>
+
