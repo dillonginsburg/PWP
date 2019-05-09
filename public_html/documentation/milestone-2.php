a2decf12e3770fed86c61cd2c939fbca05b72d9b
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-05-09 12:47:02 -0600

    new wireframes

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -14,7 +14,7 @@
 			<li><em>Portfolio:</em> This will be an image carousel showcasing my projects. Each image will link to the actual page.</li>
 			<li><em>Contact Me:</em> link to an image of my resume and my email. icon links for twitter, faceboook, and linkedin</li>
 		</ul>
-		<img src ="New%20Wireframe%201.png">
-		<img src ="New%20Wireframe%202.png">
+		<img src ="desktop-wireframe.png">
+		<img src ="mobile-wireframe.png">
 	</div>
 
