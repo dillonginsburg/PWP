00b537fed675b1c4add6a0d90b8d2d1c2fbe4747
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-05-09 12:57:26 -0600

    updated content strategy for detail

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,14 +6,16 @@
 	</head>
 	<div>
 		<h1>Content Strategy</h1>
-		<p>My site will be one page and consist of the following sections:
+		<p>My site will be 2 pages. The landing page will consist of 2 sections and a footer, and the second page will be a contact page. I want it to be minimalistic and clean.
 		</p>
+		<h2>Landing Page</h2>
 		<ul>
-			<li><em>Opening:</em> Title and subtitle, probably. It will declare my name and my occupation.</li>
-			<li><em>About Me:</em> One paragraph stating my skills and my goals</li>
+			<li><em>Top section:</em> Title and subtitle at the top, with a small blob about me and my goals below it. These will be on top of a background image.</li>
 			<li><em>Portfolio:</em> This will be an image carousel showcasing my projects. Each image will link to the actual page.</li>
-			<li><em>Contact Me:</em> link to an image of my resume and my email. icon links for twitter, faceboook, and linkedin</li>
+			<li><em>Footer:</em> link to an image of my resume and my email. icon links for twitter, faceboook, and linkedin</li>
 		</ul>
+		<h2>Contact page</h2>
+		<p>My contact page will have clean, simple buttons linking to my resume, email, and github.</p>
 		<img src ="desktop-wireframe.png">
 		<img src ="mobile-wireframe.png">
 	</div>
